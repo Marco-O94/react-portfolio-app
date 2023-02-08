748abe48a79d39cb091f22231333f39dce95b0da
Marco Oliveri <marco_oliveri@elmisoftware.com>
2023-02-08 09:17:40 +0100

    .

---
+++ src/components/Skills.tsx
@@ -0,0 +1,69 @@
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { Container, Row, Col } from "react-bootstrap";
+import meter1 from '../assets/img/meter1.svg';
+import meter2 from '../assets/img/meter2.svg';
+import meter3 from '../assets/img/meter3.svg';
+import colorSharp from '../assets/img/color-sharp.png';
+
+export const Skills = () => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
+  return (
+    <section className="skill" id="skills">
+      <Container>
+        <Row>
+          <Col>
+            <div className="skill-box">
+              <h2>Skills</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              </p>
+            </div>
+            <Carousel
+              responsive={responsive}
+              infinite={true}
+              className="skill-slider"
+            >
+              <div className="item">
+                <img src={meter1} alt="image1" />
+                <h5>Web Development</h5>
+              </div>
+              <div className="item">
+                    <img src={meter2} alt="image2" />
+                    <h5>Web Design</h5>
+                    </div>
+                    <div className="item">
+                    <img src={meter3} alt="image3" />
+                    <h5>UI/UX Design</h5>
+                    </div>
+                    <div className="item">
+                    <img src={meter1} alt="image4" />
+                    <h5>Lorem Ipsum</h5>
+                    </div>
+            </Carousel>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-left" src={colorSharp} alt="image-sharp" />
+    </section>
+  );
+};
